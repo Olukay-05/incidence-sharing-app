e1a8d1ccb9a69dd0900e7fa2bf5a4f3d9d3d8635
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2023-03-16 04:12:05 +0100

    Added InputField Component

---
+++ incidence-social-app/src/reusable-components/input-reusable/InputField.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InputField = () => {
+  return (
+    <div>
+        <input 
+            type="email"
+            name="email"
+            value="email"
+         />
+    </div>
+  )
+}
+
+export default InputField
